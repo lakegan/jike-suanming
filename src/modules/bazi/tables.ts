9786ad117b5224b442079a4f8fd39639b86e7dd3
lakegan <lakegan@gmail.com>
2026-04-07 04:42:47 +0800

    Create Bazi reference tables module with mappings for Heavenly Stems, Earthly Branches, Five Elements, and Ten Gods

---
+++ src/modules/bazi/tables.ts
@@ -0,0 +1,54 @@
+// Bazi Reference Tables
+
+// Mapping of Heavenly Stems
+export const heavenlyStems = [
+    'Jia', // 1
+    'Yi',  // 2
+    'Bing', // 3
+    'Ding', // 4
+    'Wu',   // 5
+    'Ji',   // 6
+    'Geng', // 7
+    'Xin',  // 8
+    'Ren',  // 9
+    'Gui'   // 10
+];
+
+// Mapping of Earthly Branches
+export const earthlyBranches = [
+    'Zi',  // 1
+    'Chou', // 2
+    'Yin',  // 3
+    'Mao',  // 4
+    'Chen', // 5
+    'Si',   // 6
+    'Wu',   // 7
+    'Wei',  // 8
+    'Shen', // 9
+    'You',  // 10
+    'Xu',   // 11
+    'Hai'   // 12
+];
+
+// Mapping of Five Elements
+export const fiveElements = [
+    'Wood',
+    'Fire',
+    'Earth',
+    'Metal',
+    'Water'
+];
+
+// Mapping of Ten Gods
+export const tenGods = [
+    'Direct Wealth',
+    'Indirect Wealth',
+    'Resource',
+    'Output',
+    'Eating God',
+    'Friend',
+    'Robber',
+    'Killing',
+    'Production',
+    'Dying'
+];
